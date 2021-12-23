67d5d8d2f66dbe47fe2633080d1ecc65743aff76
Andrew Nguyen <stageandrewnguyen@gmail.com>
2021-12-23 12:18:12 -0500

    added context API

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
-
 import { GetServerSideProps } from 'next';
+import { UserContext } from '../lib/context';
 
 function MyApp({ Component, pageProps }) {
   return(
     <>
+    <UserContext.Provider value={{user: {}, username: 'Andrew'}}>
      <Navbar />
      <Component {...pageProps} />
      <Toaster/>
+     </UserContext.Provider>
     </> 
-  )
+  );
 }
 
 export default MyApp
