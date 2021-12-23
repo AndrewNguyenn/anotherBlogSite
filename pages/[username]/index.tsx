8f1c8f438ce5acbc02abb2534b9f6291e8d3344b
Andrew Nguyen <stageandrewnguyen@gmail.com>
2021-12-23 18:06:01 -0500

    implemented server side rendering

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,6 +1,42 @@
-export default function Page({ }) {
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed"
+import { getUserWithUsername, postToJSON, firestore } from "../../lib/firebase";
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from 'firebase/firestore';
+
+export async function getServerSideProps({ query: urlQuery }) {
+  const { username } = urlQuery;
+  const userDoc = await getUserWithUsername(username);
+
+  if (!userDoc) {
+    return {
+      notFound: true,
+    }
+  }
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  };
+
+
+  return{ 
+    props: {user, posts}, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
+      <UserProfile user={user}/>
+      <PostFeed posts={posts} />
     </main>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,74 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@styles/Home.module.css'
+import Postfeed from '@components/PostFeed'
 import Link from 'next/link'
-import Loader from '../components/Loader'
+import Loader from '@components/Loader'
 import toast from 'react-hot-toast'
+import { firestore, postToJSON, getIt } from '@lib/firebase';
+import { Timestamp, query, where, orderBy, limit, collectionGroup, getDocs, startAfter, getFirestore, doc } from 'firebase/firestore';
+import { useState } from 'react'
 
-export default function Home() {
+
+
+const LIMIT = 10
+
+export async function getServerSideProps(context) {
+  const ref = collectionGroup(getFirestore(), 'posts');
+  const postsQuery = query(
+    ref,
+    where('published', '==', true),
+    orderBy('createdAt', 'desc'),
+    limit(LIMIT),
+  );
+  
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+  return {
+    props: { posts }, 
+  };
+}
+
+export default function Home(props: any) {
+  const [posts, setPosts] = useState[props.posts]
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+    const cursor = typeof last.createdAt === 'number' ? Timestamp.fromMillis(last.createdAt) : last.createdAt;
+    
+    const ref = collectionGroup(getFirestore(), 'posts');
+    const postsQuery = query(
+      ref,
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      startAfter(cursor),
+      limit(LIMIT),
+    );
+    const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+
+  };
+
+
+
   return (
-    <div>
-     
-    </div>
+    <main>
+      <Postfeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end.'}
+    </main>
   )
 }
