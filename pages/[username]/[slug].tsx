4315877ba36c03e29e9f633b9f5facc27a3d2ef6
Andrew Nguyen <86614261+AndrewNguyenn@users.noreply.github.com>
2021-12-24 17:30:39 -0500

    Update [slug].tsx

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -13,53 +13,53 @@ import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { useContext } from 'react';
 
 
-// export async function getStaticProps({ params }) {
-//   const { username, slug } = params;
-//   const userDoc = await getUserWithUsername(username);
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
 
-//   let post;
-//   let path;
+  let post;
+  let path;
 
-//   if (userDoc) {
-//     // const postRef = userDoc.ref.collection('posts').doc(slug);
-//     const postRef = doc(getFirestore(), userDoc.ref.path, 'posts', slug);
+  if (userDoc) {
+    // const postRef = userDoc.ref.collection('posts').doc(slug);
+    const postRef = doc(getFirestore(), userDoc.ref.path, 'posts', slug);
 
-//     // post = postToJSON(await postRef.get());
-//     post = postToJSON(await getDoc(postRef) );
+    // post = postToJSON(await postRef.get());
+    post = postToJSON(await getDoc(postRef) );
 
-//     path = postRef.path;
-//   }
+    path = postRef.path;
+  }
 
-//   return {
-//     props: { post, path },
-//     revalidate: 100,
-//   };
-// }
+  return {
+    props: { post, path },
+    revalidate: 100,
+  };
+}
 
-// export async function getStaticPaths() {
-//   // Improve my using Admin SDK to select empty docs
-//   const q = query(
-//     collectionGroup(getFirestore(), 'posts'),
-//     limit(20)
-//   )
-//   const snapshot = await getDocs(q);
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const q = query(
+    collectionGroup(getFirestore(), 'posts'),
+    limit(20)
+  )
+  const snapshot = await getDocs(q);
 
-//   const paths = snapshot.docs.map((doc) => {
-//     const { slug, username } = doc.data();
-//     return {
-//       params: { username, slug },
-//     };
-//   });
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
 
-//   return {
-//     // must be in this format:
-//     // paths: [
-//     //   { params: { username, slug }}
-//     // ],
-//     paths,
-//     fallback: 'blocking',
-//   };
-// }
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: 'blocking',
+  };
+}
 
 export default function Post(props) {
   const postRef = doc(getFirestore(), props.path);
